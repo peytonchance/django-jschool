538f94d60c61cb0fc8fcaaae10bbff0c42e748b0
peytonchance <peytonchance@gmail.com>
2018-02-28 12:56:16 -0500

    Initial commit.

---
+++ jSchool/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(unique=False, max_length=50)
+    pid = models.CharField(unique=True, max_length=12)
+    grade = models.IntegerField(unique=False)
+
+    class Meta(object):
+            ordering = ('pid', 'name')
+
+    def __str__(self):
+        return U'%s %s' %(self.name, self.pid)
+
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+    call_number = models.CharField(unique=False, max_length=4)
+    instructor = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    term = models.CharField(max_length=200)
+    date = models.DateField()
+    #students = models.ForeignKey(Student, null=True, on_delete=models.CASCADE)
+    students = models.ManyToManyField(Student)
+
+    class Meta(object):
+        verbose_name_plural = "Courses"
+        ordering = ('-date', 'name')
+
+    def __str__(self):
+        return U'%s %s' %(self.call_number, self.name)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.upper()
+        super(Course, self).save(*args, **kwargs)
